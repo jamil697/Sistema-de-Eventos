6360c864c659136af5928efcf7501b43e81a6b5d
Lady <2022110475@udh.edu.pe>
2025-11-18 15:28:26 -0500

    Vistas necesarias para el proyecto

---
+++ resources/views/ciudadano/detalle.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h2>Detalle del Evento</h2>
+
+    <div class="card mt-3">
+        <div class="card-body">
+            <h4>Festival Cultural</h4>
+            <p><strong>Fecha:</strong> 10/12/2025</p>
+            <p><strong>Ubicación:</strong> Plaza Mayor</p>
+            <p><strong>Categoría:</strong> Cultural</p>
+
+            <p>
+                Este festival reúne actividades artísticas, música en vivo y exposición de artesanía local.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat venenatis lectus.
+            </p>
+
+            <a href="#" class="btn btn-primary">Inscribirme</a>
+        </div>
+    </div>
+</div>
+@endsection
