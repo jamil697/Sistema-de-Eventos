9776b66e9ae9c6f73741abce3e74f0b5b389a889
Lady <2022110475@udh.edu.pe>
2025-12-03 09:59:45 -0500

    Mejorar en las rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/', function () { return view('welcome'); });
 
@@ -35,30 +40,28 @@ Route::get('/admin/recursos/asignar', function () {
     return view('admin.recursos.asignar');
 })->name('admin.recursos.asignar');
 
-Route::get('/admin/eventos/recursos', function () {
-    return view('admin.recursos.asignar');
-})->name('admin.eventos.recursos');
 
 /* Ciudadano - inscripciones (rutas únicas, NO repetir la misma URI) */
-Route::get('/ciudadano/inscripciones/buscar', function () {
-    return view('ciudadano.inscripciones.buscar');
-})->name('ciudadano.inscripciones.buscar');
+Route::get('/ciudadano/buscar', function () {
+    return view('ciudadano.inscripcion');
+})->name('ciudadano.buscar');
 
-Route::get('/ciudadano/inscripciones/detalle', function () {
-    return view('ciudadano.inscripciones.detalle');
-})->name('ciudadano.inscripciones.detalle');
+Route::get('/ciudadano/detalle', function () {
+    return view('ciudadano.detalle');
+})->name('ciudadano.detalle');
 
-Route::get('/ciudadano/inscripciones/inscripcion', function () {
-    return view('ciudadano.inscripciones.inscripcion');
-})->name('ciudadano.inscripciones.inscripcion');
+Route::get('/ciudadano/inscripcion', function () {
+    return view('ciudadano.inscripcion');
+})->name('ciudadano.inscripcion');
 
-Route::get('/ciudadano/inscripciones/lista', function () {
-    return view('ciudadano.inscripciones.lista_inscripcion');
-})->name('ciudadano.inscripciones.lista_inscripcion');
+Route::get('/ciudadano/lista', function () {
+    return view('ciudadano.lista_inscripcion');
+})->name('ciudadano.lista_inscripcion');
 
-Route::get('/ciudadano/inscripciones/mostrar', function () {
-    return view('ciudadano.inscripciones.mostrar');
-})->name('ciudadano.inscripciones.mostrar');
+Route::get('/ciudadano/mostrar', function () {
+    return view('ciudadano.mostrar');
+})->name('ciudadano.mostrar');
+
 
 Auth::routes();
 
