52c7a4b68b29cc75c93979e631482683cb726d91
Jamil <estradariverajamil@gmail.com>
2025-12-04 18:33:21 -0500

    Guardando cambios antes de actualizar main

---
+++ resources/views/events/inscripcion.blade.php
@@ -0,0 +1,36 @@
+
+@extends('layouts.app')
+
+@section('content')
+@if(Auth::user()->role === 'ciudadano')
+    <div class="container">
+        <h2>Inscripción al Evento</h2>
+
+        <div class="card mt-3">
+            <div class="card-body">
+
+                <h4>Formulario de Inscripción</h4>
+
+                <form>
+                    <div class="mb-3">
+                        <label>Nombre completo</label>
+                        <input type="text" class="form-control">
+                    </div>
+
+                    <div class="mb-3">
+                        <label>DNI</label>
+                        <input type="text" class="form-control">
+                    </div>
+
+                    <div class="mb-3">
+                        <label>Correo electrónico</label>
+                        <input type="email" class="form-control">
+                    </div>
+
+                    <button class="btn btn-success">Enviar Inscripción</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endif
+@endsection
